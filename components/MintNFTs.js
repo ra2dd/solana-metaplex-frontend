4ca90206a4c4edf1379941d4ead5814f19946bf1
ra2dd <radoslawpryw@gmail.com>
2023-12-08 16:55:25 +0100

    add minting functionality

--- components/MintNFTs.js
+++ components/MintNFTs.js
@@ -1,40 +1,71 @@
-import { useState } from "react";
-import { Metaplex, createAuctionHouseBuilder } from "@metaplex-foundation/js";
+import { useState, useMemo, useCallback } from "react";
 import * as web3 from "@solana/web3.js";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import { generateSigner, transactionBuilder, some } from "@metaplex-foundation/umi";
+import { fetchCandyMachine, mintV2, mplCandyMachine, safeFetchCandyGuard } from "@metaplex-foundation/mpl-candy-machine";
+import { walletAdapterIdentity } from '@metaplex-foundation/umi-signer-wallet-adapters';
+import { setComputeUnitLimit } from "@metaplex-foundation/mpl-toolbox";
+import * as bs58 from "bs58";
 
 export function MintNFTs() {
   const { connection } = useConnection();
-  const { publicKey } = useWallet();
+  const wallet = useWallet();
   const [nft, setNft] = useState(null);
 
-  const metaplex = Metaplex.make(connection);
+  const endpoint = web3.clusterApiUrl('devnet');
   const candyMachineAddress = new web3.PublicKey(process.env.NEXT_PUBLIC_CANDY_MACHINE_ID);
-
-  let candyMachine;
-  let walletBalance;
-
-  const checkEligibility = async () => {
-    candyMachine = await metaplex
-    .candyMachines()
-    .findByAddress({ address: candyMachineAddress})
-  }
+  const treasury = new web3.PublicKey(process.env.NEXT_PUBLIC_TREASURY);
 
   const handleClick = async () => {
-    if (!connection || !publicKey) {
+    if (!connection || !wallet) {
       alert('Connect solana wallet.')
     }
-    
-    await checkEligibility();
-    console.log(candyMachine)
 
-    const { nft } = await metaplex.candyMachines().mint({
-      candyMachine,
-      collectionUpdateAuthority: candyMachine.authorityAddress
-    });
+    try {
+      const umi = createUmi(endpoint)
+        .use(walletAdapterIdentity(wallet))
+        .use(mplCandyMachine())
 
-    console.log(nft);
-    setNft(nft);
+      const candyMachine = await fetchCandyMachine(
+        umi,
+        candyMachineAddress
+      );
+      // console.log(candyMachine);
+
+      const candyGuard = await safeFetchCandyGuard(
+        umi,
+        candyMachine.mintAuthority,
+      );
+
+      const nftMint = generateSigner(umi);
+
+      const transaction = await transactionBuilder()
+        .add(setComputeUnitLimit(umi, { units: 800_000 }))
+        .add(
+          mintV2(umi, {
+            candyMachine: candyMachine.publicKey,
+            candyGuard: candyGuard?.publicKey,
+            nftMint,
+            collectionMint: candyMachine.collectionMint,
+            collectionUpdateAuthority: candyMachine.authority,
+            tokenStandard: candyMachine.tokenStandard,
+            mintArgs: {
+              solPayment: some({ destination: treasury }),
+            }
+          })
+        )
+
+      const { signature } = await transaction.sendAndConfirm(umi, {
+        confirm: { commitment: "confirmed" },
+      });
+      const txid = bs58.encode(signature);
+      console.log(`Success, mint: ${txid}`);
+      alert('Mint successful');
+    } catch(error) {
+      console.log(error)
+      alert(JSON.stringify(error));
+    }
   }
 
   return(
